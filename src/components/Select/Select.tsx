307c5efaac48f18eb92ffad253807b1e65ac9568
Alteri <trochonowicz.jacek@gmail.com>
2022-07-02 12:44:34 +0200

    add components Select

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+import { Label } from "./styled";
+
+type SelectProps = {
+  name: string;
+  label?: string;
+  options: { value: string; label: string }[];
+};
+
+const Select = ({ name, label, options }: SelectProps) => {
+  return (
+    <Label gap="4" as="label">
+      {label && <p>{label}</p>}
+      <Controller
+        name={name}
+        render={({ field }) => {
+          return (
+            <select
+              defaultValue={field.value}
+              onChange={(e) => field.onChange(e.target.value)}
+            >
+              {options.map(({ value, label }) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
+            </select>
+          );
+        }}
+      />
+    </Label>
+  );
+};
+
+export default Select;
---
+++ src/components/Select/styled.ts
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+import { GridStyled } from "../Grid/styled";
+
+export const Label = styled(GridStyled)``;
